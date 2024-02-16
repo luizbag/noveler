420897c39015ecba26355e9d13a8a338c0640612
Luiz Gavioli <428691+luizbag@users.noreply.github.com>
2024-02-16 21:39:44 +0000

    Bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
-import { metadata } from "./layout";
+'use client'
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import './page.module.css';
 
 export default function Home() {
   return (
-    <>
-      <h1>{metadata.title}</h1>
-      <p>{metadata.description}</p>
-    </>
+
+    <div className="d-flex h-100 vh-100 text-center text-white bg-dark">
+
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header className="mb-auto">
+          <div>
+            <h3 className="float-md-start mb-0">Noveler</h3>
+            <Nav className="nav-masthead justify-content-center float-md-end">
+              <Nav.Link className="active" aria-current="page" href="#">Home</Nav.Link>
+              <Nav.Link href="#">Features</Nav.Link>
+              <Nav.Link href="#">Contact</Nav.Link>
+            </Nav>
+          </div>
+        </header>
+
+        <main className="px-3">
+          <h1>Noveler</h1>
+          <p className="lead">A place to write.</p>
+          <p className="lead">
+            <Button href="#" className="btn-lg btn-secondary fw-bold border-white bg-white text-black">Learn more</Button>
+          </p>
+        </main>
+
+        <footer className="mt-auto text-white-50">
+          <p>Cover template for <a href="https://getbootstrap.com/" className="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" className="text-white">@mdo</a>.</p>
+        </footer>
+      </div>
+    </div>
   );
 }
